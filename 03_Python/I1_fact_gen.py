38b53d98ed4b3a48e68c839ba61c2625c0fe2246
ray13542 <ray13542@gmail.com>
2023-12-14 15:27:01 +0800

    I0, I1 test another pattern

--- 03_Python/I1_fact_gen.py
+++ 03_Python/I1_fact_gen.py
@@ -6,11 +6,11 @@ def fact(n):
 
 if __name__ == '__main__':
     # Modify your test pattern here
-    n = 5
+    n = 10
         
-    with open('../00_TB/Pattern/I1/mem_D.dat', 'w') as f_data:
+    with open('00_TB\Pattern\I1\mem_D.dat', 'w') as f_data:
         f_data.write(f"{n:08x}\n")
 
-    with open('../00_TB/Pattern/I1/golden.dat', 'w') as f_ans:
+    with open('00_TB\Pattern\I1\golden.dat', 'w') as f_ans:
         f_ans.write('{:0>8x}\n'.format(n))
         f_ans.write('{:0>8x}\n'.format(fact(n)))
